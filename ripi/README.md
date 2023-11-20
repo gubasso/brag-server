70c7f55fbe99af162a0bca1a235738013870a66a
Gustavo Basso <gubasso@eambar.net>
2023-11-20 15:57:47 -0300

    (up) Issue #setup_basic_axum_db_structure.

--- ripi/README.md
+++ ripi/README.md
@@ -1,4 +1,16 @@
 # ripissue drafts
 
-- [Introduction to Axum - Brooks Builds - Playlist](https://www.youtube.com/playlist?list=PLrmY5pVcnuE-_CP7XZ_44HN-mDrLQV4nS)
+- toml config
+  - all repositories
+    - which branch
+  - all contributors (e-mail, or name)
+  - load ssh-agent (keychain as a dependency)
+  - clone repositories
+  - update repos routine (push)
+  - from repo to json
+  - filter and aggregate data
+  - save to db
+- initial server setup (spin up)
+  - systemctl inside rust?
+- server up, serving routes and repo updates routines
 - Follow examples at: https://github.com/tokio-rs/axum/tree/main/examples
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,14 @@
-use axum::{
-    http::StatusCode,
-    response::{Html, IntoResponse},
-    routing::get,
-    Router, Server,
-};
-use listenfd::ListenFd;
-use std::net::SocketAddr;
-
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(handler))
-        .nest("/error", api_error());
-    let mut listenfd = ListenFd::from_env();
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let server = match listenfd.take_tcp_listener(0).unwrap() {
-        Some(listener) => Server::from_tcp(listener).unwrap(),
-        None => Server::bind(&addr),
-    };
-    println!("listening on {}", addr);
-    server.serve(app.into_make_service()).await.unwrap();
-}
-
-struct AppError(anyhow::Error);
-
-impl IntoResponse for AppError {
-    fn into_response(self) -> axum::response::Response {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
-        )
-            .into_response()
-    }
-}
-
-impl<E> From<E> for AppError
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(err: E) -> Self {
-        Self(err.into())
-    }
-}
-
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Helloooooww, woooorrrlldd!!</h1>")
-}
-
-fn api_error() -> Router {
-    Router::new().route("/", get(handle_error))
-}
-
-async fn handle_error() -> Result<(), AppError> {
-    try_thing()?;
-    Ok(())
-}
-
-fn try_thing() -> Result<(), anyhow::Error> {
-    anyhow::bail!("it failed!")
+    // let app = Router::new()
+    //     .route("/", get(handler))
+    //     .nest("/error", api_error());
+    // let mut listenfd = ListenFd::from_env();
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // let server = match listenfd.take_tcp_listener(0).unwrap() {
+    //     Some(listener) => Server::from_tcp(listener).unwrap(),
+    //     None => Server::bind(&addr),
+    // };
+    // println!("listening on {}", addr);
+    // server.serve(app.into_make_service()).await.unwrap();
 }
