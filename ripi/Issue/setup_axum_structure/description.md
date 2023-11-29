fb06ceec84ee7a4d5254f1d2af609bdf2c4e4823
Gustavo Basso <gubasso@eambar.net>
2023-11-29 18:07:52 -0300

    (up) Issue #setup_axum_structure.

--- ripi/Issue/setup_axum_structure/description.md
+++ ripi/Issue/setup_axum_structure/description.md
@@ -1,1 +1,3 @@
 # setup_axum_structure (Issue)
+
+- [x] intro_to_axum repo
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@ mod queries;
 mod types;
 mod utils;
 
-use std::{env, error::Error};
+use std::{env, error::Error, net::SocketAddr};
 
+use axum::{routing::get, Router, Server};
 use queries::insert_commits_to_db;
 use tokio::fs::create_dir_all;
+use tower_http::cors::{Any, CorsLayer};
 use types::repositories::Repositories;
 use utils::{load_config, repos_base_path};
 
 const HOME: &str = env!("HOME");
 const REPOS_BASE_PATH: &str = "/.local/share/brag-server/repos";
 const CARGO_PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+async fn root() -> &'static str {
+    "Hello, World!!!!"
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -30,16 +36,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let repositories = Repositories::from(&config.hosts).await?;
     insert_commits_to_db(&pool, &repositories).await?;
 
-    // let app = Router::new()
-    //     .route("/", get(handler))
-    //     .nest("/error", api_error());
-    // let mut listenfd = ListenFd::from_env();
-    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    // let server = match listenfd.take_tcp_listener(0).unwrap() {
-    //     Some(listener) => Server::from_tcp(listener).unwrap(),
-    //     None => Server::bind(&addr),
-    // };
-    // println!("listening on {}", addr);
-    // server.serve(app.into_make_service()).await.unwrap();
+    let cors = CorsLayer::new().allow_origin(Any);
+    let app = Router::new()
+        .route("/", get(root))
+        .with_state(pool)
+        .layer(cors);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    Server::bind(&addr).serve(app.into_make_service()).await?;
     Ok(())
 }
